14d03ccf76e55909353e9e0e93198de8f7ca64a1
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-03-15 19:01:29 +0100

    fix(posible heureka)

--- src/app/servicio/cabecera.service.ts
+++ src/app/servicio/cabecera.service.ts
@@ -22,6 +22,7 @@ export class CabeceraService {
         let headers = new HttpHeaders();
         headers = headers.append("Authorization", "Basic " + btoa(environment.login.user+":"+environment.login.pass));
         headers = headers.append("Content-Type", "application/x-www-form-urlencoded");
+        headers = headers.append("Access-Control-Allow-Origin", '*');
 
         return headers;
     }
