d08ccbb455533a79960d41a5db6f3592e459ed0c
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-03-15 17:07:39 +0100

    fix(server node)

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8080;
 const server = require('http').Server(app);
+const request = require('request');
 
 const dist_dir = "/../dist/scrume-front";
+
+const back = "https://api-scrume.cleverapps.io/api";
+const front = "https://scrume-brand.herokuapp.com";
+const cosr = "Access-Control-Allow-Origin";
 
 app.use(express.static(__dirname + dist_dir));
 
@@ -13,12 +18,26 @@ server.listen(port, function() {
     console.log("Current dir: "+__dirname + dist_dir);
 })
 
+app.use((req, res, next) => {
+    res.header(cors, "*");
+    next();
+});
+
 // PathLocationStrategy
 
 app.get('', function(req, res) {
+
     res.sendFile(path.join(__dirname + dist_dir+'/index.html'));
 });
 
 app.get('/', function(req, res) {
+
     res.sendFile(path.join(__dirname + dist_dir+'/index.html'));
+});
+
+app.all('/api/*', function(req, res, next){
+    let url = req.originalUrl.split(front);
+    console.log(url);
+    request(front);
+    next();
 });
