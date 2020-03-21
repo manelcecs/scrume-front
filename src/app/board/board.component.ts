33da1e16c6b036ac5bcce0ce90c6e4cb36c548a8
Ezequiel Portillo Jurado <ezequiel6a@gmail.com>
2020-03-21 19:19:05 +0100

    fix(board): Funcionalidad completa de mover tareas

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -19,6 +19,7 @@ export class BoardComponent implements OnInit {
   idBoard: number;
   task: TaskMove;
   n: number;
+  taskSend: TaskMove;
 
   constructor(private router: Router, private boardService: BoardService, private activatedRoute: ActivatedRoute, private taskservice: TaskService) { }
 
@@ -27,7 +28,6 @@ export class BoardComponent implements OnInit {
      this.activatedRoute.queryParams.subscribe(param => {
 
        if(param.id != undefined){
-         //TODO
          this.idBoard = param.id;
 
           this.boardService.getBoard(this.idBoard).subscribe((board:Board)=>{
@@ -61,15 +61,20 @@ export class BoardComponent implements OnInit {
       let col2 = new String(event.container.data);
 
       if (col == "To Do" && col2 == "In progress") {
-        this.transferTaskToArray(this.board.columns[0].tasks, this.board.columns[1].tasks, event.previousIndex, event.currentIndex);
+        this.n = this.transferTaskToArray(this.board.columns[0].tasks, this.board.columns[1].tasks, event.previousIndex, event.currentIndex);
+        this.moveTask(this.board.columns[1].id, this.n);
       } else if (col == "To Do" && col2 == "Done"){
-        this.transferTaskToArray(this.board.columns[0].tasks, this.board.columns[2].tasks, event.previousIndex, event.currentIndex);
+        this.n = this.transferTaskToArray(this.board.columns[0].tasks, this.board.columns[2].tasks, event.previousIndex, event.currentIndex);
+        this.moveTask(this.board.columns[2].id, this.n);
       } else if (col == "In progress" && col2 == "To Do"){
-        this.transferTaskToArray(this.board.columns[1].tasks, this.board.columns[0].tasks, event.previousIndex, event.currentIndex);
+        this.n = this.transferTaskToArray(this.board.columns[1].tasks, this.board.columns[0].tasks, event.previousIndex, event.currentIndex);
+        this.moveTask(this.board.columns[0].id, this.n);
       } else if (col == "In progress" && col2 == "Done"){
-        this.transferTaskToArray(this.board.columns[1].tasks, this.board.columns[2].tasks, event.previousIndex, event.currentIndex);
+        this.n = this.transferTaskToArray(this.board.columns[1].tasks, this.board.columns[2].tasks, event.previousIndex, event.currentIndex);
+        this.moveTask(this.board.columns[2].id, this.n);
       } else if (col == "Done" && col2 == "To Do"){
-        this.transferTaskToArray(this.board.columns[2].tasks, this.board.columns[0].tasks, event.previousIndex, event.currentIndex);
+        this.n = this.transferTaskToArray(this.board.columns[2].tasks, this.board.columns[0].tasks, event.previousIndex, event.currentIndex);
+        this.moveTask(this.board.columns[0].id, this.n);
       }else{
         this.n = this.transferTaskToArray(this.board.columns[2].tasks, this.board.columns[1].tasks, event.previousIndex, event.currentIndex);
         this.moveTask(this.board.columns[1].id, this.n);
@@ -118,10 +123,11 @@ navigateTo(route: String): void{
   this.router.navigate([route]);
 }
 
-moveTask(idDest: Number, idtask: number):Observable<TaskMove> {
-  this.task = {destiny: idDest, task: idtask};
-  console.log("la task a enviar " + JSON.stringify(this.task))
-  return this.taskservice.moveTask(this.task);
+moveTask(idDest: Number, idtask: number): void {
+  this.taskSend = {destiny: idDest, task: idtask};
+  this.taskservice.moveTask(this.taskSend).subscribe((task : TaskMove) => {
+    console.log(JSON.stringify(task));
+  });
 }
 
 onClick(event): any {
