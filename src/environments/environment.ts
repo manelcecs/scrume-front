28be133836de603e51b9a06428f7f351d2cf6807
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-03-22 18:30:20 +0100

    fix cambio contraseña prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   "login": {
     "user": "testuser@gmail.com",
     "pass": "1234563"
