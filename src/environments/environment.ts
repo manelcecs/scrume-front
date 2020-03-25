714b254b4ea202331908dcfb008c7e340bd5f9f1
Antonio <antcarluq@alum.us.es>
2020-03-25 11:02:22 +0100

    fix (API): Se ha corregido la URL de la API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     "user": "testuser@gmail.com",
     "pass": "1234563"
   },
-  "backend": 'https://api-scrume-s2.cleverapps.io/'
+  "backend": 'https://api-scrume-sp2.herokuapp.com/'
 };
 
 /*'https://api-scrume.cleverapps.io/'
