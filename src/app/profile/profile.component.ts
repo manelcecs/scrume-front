31b1d4b7a8db1a1b1ce90c2c6c08e7151038a8e9
Ezequiel Portillo Jurado <ezequiel6a@gmail.com>
2020-03-28 13:21:24 +0100

    feat(profile): Editar datos del user (no userAccount)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../servicio/user.service';
 import { ProfileService } from '../servicio/profile.service';
 import { UserNick } from '../dominio/user.domain';
 import { Profile } from '../dominio/profile.domain';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -13,16 +15,67 @@ export class ProfileComponent implements OnInit {
 
   profile: Profile;
 
-  constructor(private userService: UserService, private profileService: ProfileService) { }
+  name: FormControl = new FormControl('',{validators: [Validators.required]});
+  nick: FormControl = new FormControl('',{validators: [Validators.required]});
+  surnames: FormControl = new FormControl('',{validators: [Validators.required]});
+  photo: FormControl = new FormControl('');
+  gitUser: FormControl = new FormControl('');
+
+  constructor(private userService: UserService, private profileService: ProfileService, private router: Router) { }
 
   ngOnInit(): void {
 
     this.userService.findUserAuthenticated().subscribe((user: UserNick)=>{
       this.profileService.getProfile(user.idUser).subscribe((profile: Profile)=>{
         this.profile = profile;
+
         console.log(JSON.stringify(this.profile));
+
+        this.name.setValue(this.profile.name);
+        this.nick.setValue(this.profile.nick);
+        this.surnames.setValue(this.profile.surnames);
+        this.photo.setValue(this.profile.photo);
+        this.gitUser.setValue(this.profile.gitUser);
+
       });
     });
+
+  }
+
+  editProfile(){
+    
+    this.profile.name = this.name.value;
+    this.profile.nick = this.nick.value;
+    this.profile.photo = this.photo.value;
+    this.profile.surnames = this.surnames.value;
+    this.profile.gitUser = this.gitUser.value;
+
+    this.profile = { id: this.profile.id,
+      idUserAccount: this.profile.idUserAccount,
+      gitUser: this.profile.gitUser,
+      name: this.profile.name,
+      nick: this.profile.nick,
+      photo: this.profile.photo,
+      surnames: this.profile.surnames
+    }
+
+    this.profileService.editProfile(this.profile).subscribe((pro: Profile)=> {
+      this.profile = pro;
+      this.router.navigate(['teams']);
+    });
+
+  }
+
+  cancelEditProfile(){
+    this.router.navigate(['teams']);
+  }
+
+  validForm():boolean {
+
+    let valid: boolean = true;
+
+    valid = valid && this.name.valid && this.nick.valid && this.surnames.valid;
+    return valid;
 
   }
 
